c8d9a27f85c663d2ab2183de8ab98a8852a9e41a
Ing Samuel Gutiérrez <149992115+SamuelGutierrezC@users.noreply.github.com>
2024-04-15 15:28:38 -0600

    Add files via upload

---
+++ MyLinkedList/ejeplo.py
@@ -0,0 +1,7 @@
+import MyLinked as lb
+
+lista_enlazada= lb.linked_list ()
+
+lista_enlazada.add_at_front (6)
+lista_enlazada.add_at_end (5)
+lista_enlazada.print_list ()
---
+++ MyLinkedList/setup.py
@@ -0,0 +1,8 @@
+from setuptools import find_packages, setup
+
+setup(
+    name= 'MyLinked',
+    version= '0.0.1',
+    packages= find_packages(),
+    py_modules= ["MyLinked"],
+    )
